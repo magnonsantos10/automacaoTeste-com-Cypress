2aa297260d168c00a691f7e33f3c11e690b2f4a5
magnonsantos1 <felipemagnon@gmail.com>
2021-12-20 22:09:43 -0300

    add allure-report ao projeto

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,19 +1,9 @@
 /// <reference types="cypress" />
 /// <reference types="@shelex/cypress-allure-plugin" />
 
-const allureWriter = require('@shelex/cypress-allure-plugin/writer');
-// import allureWriter from "@shelex/cypress-allure-plugin/writer";
+const allureWriter = require("@shelex/cypress-allure-plugin/writer")
 
 module.exports = (on, config) => {
-    allureWriter(on, config);
-    return config;
-};
-
-/**
- * @type {Cypress.PluginConfig}
- */
-// eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  allureWriter(on, config);
+  return config;
 }
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,4 +1,3 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import '@shelex/cypress-allure-plugin'
-require('@shelex/cypress-allure-plugin')
